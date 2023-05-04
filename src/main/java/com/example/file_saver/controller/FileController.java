b997b24b6e6c046f90e505f345986514906ed99e
DESKTOP-FOVJ9PU\Advance <usman.iqbal8899@gmail.com>
2023-05-03 22:33:33 -0700

    all

--- src/main/java/com/example/file_saver/controller/FileController.java
+++ src/main/java/com/example/file_saver/controller/FileController.java
@@ -1,14 +1,20 @@
 package com.example.file_saver.controller;
 
 import com.example.file_saver.entity.FileModel;
+import com.example.file_saver.repository.FileRepository;
 import com.example.file_saver.service.FileService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.awt.*;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -17,6 +23,9 @@ public class FileController {
 
     @Autowired
     private FileService fileService;
+
+    @Autowired
+    private FileRepository fileRepository;
 
     @PostMapping("/upload")
     public ResponseEntity<FileModel> saveFile(@RequestParam("file")MultipartFile file){
@@ -30,6 +39,14 @@ public class FileController {
 
     @GetMapping("/{id}")
     public ResponseEntity<byte[]> downloadFile(@PathVariable Long id) {
-        return fileService.getFile(id);
+        Optional<FileModel> file = fileRepository.findById(id);
+        if (file.isPresent()){
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.parseMediaType(file.get().getContentType()));
+            return new ResponseEntity<>(file.get().getData(), headers,HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
     }
 }
--- src/main/java/com/example/file_saver/service/FileService.java
+++ src/main/java/com/example/file_saver/service/FileService.java
@@ -3,15 +3,10 @@ package com.example.file_saver.service;
 import com.example.file_saver.entity.FileModel;
 import com.example.file_saver.repository.FileRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.util.Optional;
 
 @Service
 public class FileService {
@@ -25,19 +20,6 @@ public class FileService {
         fileModel.setFileName(file.getOriginalFilename());
         fileModel.setData(file.getBytes());
         return fileRepository.save(fileModel);
-    }
-
-    public ResponseEntity<byte[]> getFile(Long id) {
-        Optional<FileModel> fileOptional = fileRepository.findById(id);
-        if (fileOptional.isPresent()) {
-            FileModel file = fileOptional.get();
-            HttpHeaders headers = new HttpHeaders();
-            headers.setContentType(MediaType.parseMediaType(file.getContentType()));
-            headers.setContentDispositionFormData(file.getFileName(), file.getFileName());
-            return new ResponseEntity<>(file.getData(), headers, HttpStatus.OK);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
     }
 
 }
